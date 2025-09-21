71b178ec57475c99aa5a389588317a12280dcbfc
Vincent <54863162+gitVincentT@users.noreply.github.com>
2025-09-21 15:19:52 -0400

    Add files via upload

---
+++ wordsIKnow.py
@@ -0,0 +1,13 @@
+'''
+This app records all the words that you know alphabetically
+1.Figure out how to store inputs into a dictionary, array or list
+2. type list by alphabet
+3. Figure out local storage in python
+'''
+print('Think of all the words that start with "a", how many can you think of?')
+alist =[]
+for i in range(2):
+    enter = input()
+    alist.append(enter)
+
+print(alist)
