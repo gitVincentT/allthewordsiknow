4f49492c7d2418f81e98fc7e4c56a39d4fa70394
Vincent <54863162+gitVincentT@users.noreply.github.com>
2025-09-22 18:10:20 -0400

    Update wordsIKnow.py

--- wordsIKnow.py
+++ wordsIKnow.py
@@ -1,14 +1,28 @@
-print('Think of all the words that start with "a", how many can you think of?')
+choice = input('Think of all the words that start with a letter, starting with a,b, or c. Choose a letter')
 alist =[]
-enter = []
-while True:
-    enter = input()
-    if enter == "!":
-        break
-    elif enter[0] != 'a':
-        print('That letter doesn\'t start with a')
-    elif enter in alist:
-                print('Already entered.')
-    else:
-        alist.append(enter)
-        print(alist)    
+blist =[]
+clist =[]
+enter =[]
+
+def entry():
+    while True:
+        enter = input()
+        if enter == "!":
+            break
+        elif enter[0] != choice:
+            print('That letter doesn\'t start with ' + choice)
+        elif enter in alist or enter in blist or enter in clist:
+            print('Already entered.')
+        elif choice == 'a':
+            alist.append(enter)
+            print(alist)    
+        elif choice == 'b':
+            blist.append(enter)
+            print(blist)  
+        elif choice == 'c':
+            clist.append(enter)
+            print(clist)
+        else:
+            print('I don\'t know what you wrote!')
+
+entry()
